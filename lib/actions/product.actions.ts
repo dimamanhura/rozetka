e09a068eb8d09496cb253ca4a5f3f69c1c6ee26f
Manhura Dima <manguradima@gmail.com>
2025-08-15 07:45:03 +0300

    Add product reviews

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -36,11 +36,17 @@ export async function getAllProducts({
   limit = PAGE_SIZE,
   page,
   category,
+  price,
+  rating,
+  sort,
 }: {
   query?: string;
-  limit: number;
+  limit?: number;
   page: number;
   category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
 }) {
   const queryFilter: Prisma.ProductWhereInput = query && query !== 'all'
     ? ({
@@ -50,18 +56,53 @@ export async function getAllProducts({
         }
       })
     : {};
-  
+
+  const categoryFilter: Prisma.ProductWhereInput = category && category !== 'all'
+    ? { category }
+    : {};
+
+  const priceFilter: Prisma.ProductWhereInput = price && price !== 'all'
+    ? ({ 
+        price: {
+          gte: Number(price.split('-')[0]),
+          lte: Number(price.split('-')[1]),
+        },
+      })
+    : {};
+
+  const ratingFilter: Prisma.ProductWhereInput = rating && rating !== 'all'
+    ? ({ 
+        rating: {
+          gte: Number(rating),
+        },
+      })
+    : {};
+
   const data = await prisma.product.findMany({
-    where: queryFilter,
+    where: {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    },
     skip: (page - 1) * limit,
     take: limit,
-    orderBy: {
-      createdAt: 'desc',
-    },
+    orderBy: sort === 'lowest'
+      ? { price: 'asc' }
+      : sort === 'highest'
+        ? { price: 'desc' }
+        : sort === 'rating'
+          ? { rating: 'desc' }
+          : { createdAt: 'desc' },
   })
 
   const dataCount = await prisma.product.count({
-    where: queryFilter,
+    where: {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    },
   });
 
   return {
@@ -145,4 +186,25 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
       message: formatErrors(err),
     };
   }
-}
+};
+
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: true,
+  });
+  return data;
+};
+
+export async function getFeaturedProducts() {
+  const featuredProducts = await prisma.product.findMany({
+    where: {
+      isFeatured: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    take: 4
+  });
+  return convertToPlainObject(featuredProducts);
+};
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -16,7 +16,7 @@ export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
 
 export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
 
-export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 10;
 
 export const USER_ROLES = process.env.USER_ROLES
   ? process.env.USER_ROLES.split(', ')
